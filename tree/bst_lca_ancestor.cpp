36f868cd7310ec1e20395bd6c7e34e19073311c3
acttell <hakkiri@163.com>
2019-12-06 16:52:45 +0800

    Update bst_lca_ancestor.cpp

--- tree/bst_lca_ancestor.cpp
+++ tree/bst_lca_ancestor.cpp
@@ -1,3 +1,4 @@
+//https://www.cnblogs.com/grandyang/p/4641968.html
 //https://www.cnblogs.com/grandyang/p/4640572.html
 #include<iostream>
 #include<stack>
@@ -29,6 +30,14 @@ tree* ancestor_bst(tree* root,tree* p,tree* q)
     else if(root->val<min(p->val,q->val)) return ancestor_bst(root->right,p,q);
     else return root;
 }
+tree* ancestor_common_tree(tree* root,tree* p,tree* q)
+{
+    if(root==NULL || p==root || q==root) return root;
+    tree* left=ancestor_common_tree(root->left,p,q);
+    tree* right=ancestor_common_tree(root->right,p,q);
+    if(left!=NULL && right!=NULL) return root;
+    return left!=NULL?left:right;
+}
 int main()
 {
     int a[11]={4,2,1,'#','#',3,'#','#',6,'#','#'};
@@ -41,5 +50,7 @@ int main()
     tree* q=root->left->right;
     tree* t=ancestor_bst(root,p,q);
     cout<<"lowest common ancestor bst:"<<t->val<<endl;
+    tree* t1=ancestor_common_tree(root,p,q);
+    cout<<"LCA t1:"<<t1->val<<endl;
     return 0;
 }
