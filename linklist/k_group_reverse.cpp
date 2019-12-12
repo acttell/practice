de95c7cfd9d1922c5e1af31bb961e9564a736ecf
acttell <hakkiri@163.com>
2019-12-12 22:36:52 +0800

    Create k_group_reverse.cpp

---
+++ linklist/k_group_reverse.cpp
@@ -0,0 +1,80 @@
+//https://www.cnblogs.com/grandyang/p/4254860.html
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<queue>
+#include<cmath>
+#include<limits.h>
+using namespace std;
+typedef struct Node
+{
+    struct Node* next;
+    int val;
+}node;
+node* revert(node* h)
+{
+    node* p=h;
+    node* q=h->next;
+    node* r=q->next;
+    h->next=NULL;
+    while(q!=NULL)
+    {
+        q->next=p;
+        p=q;
+        q=r;
+        if(r!=NULL) r=r->next;
+    }
+    return p;
+}
+node* k_group_reverse(node* head,int k)
+{
+    node* dummy=new node;
+    node* pre=dummy;
+    node* cur=dummy;
+    dummy->next=head;
+    int num=0;
+    while(cur->next!=NULL)
+    {
+        num++;
+        cur=cur->next;
+    }
+    while(num>=k)
+    {
+        cur=pre->next;
+        for(int i=1;i<k;i++)
+        {
+            node* t=cur->next;
+            cur->next=t->next;
+            t->next=pre->next;
+            pre->next=t;
+        }
+        pre=cur;
+        num-=k;
+    }
+    return dummy->next;
+}
+int main()
+{
+    node* n1=new node;
+    node* n2=new node;
+    node* n3=new node;
+    node* n4=new node;
+    node* n5=new node;
+    n1->val=1;
+    n2->val=2;
+    n3->val=3;
+    n4->val=4;
+    n5->val=5;
+    n1->next=n2;
+    n2->next=n3;
+    n3->next=n4;
+    n4->next=n5;
+    n5->next=NULL;
+    node* h=k_group_reverse(n1,3);
+    while(h!=NULL)
+    {
+        cout<<h->val<<",";
+        h=h->next;
+    }
+    return 0;
+}
